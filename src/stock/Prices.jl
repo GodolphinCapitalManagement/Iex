043a8a5123b419852ec9a4276fb025d6fe0610c1
Gyan Sinha <gsinha@godolphincapital.com>
2020-07-24 12:39:48 -0400

    added new functions; added empty dataframe with defined types

--- src/stock/Prices.jl
+++ src/stock/Prices.jl
@@ -1,46 +1,77 @@
 # Stocks/Prices
 
-import Dates: Date
-import DataFrames: DataFrame
 
-import Iex.Connect: get_stocks, status
+import Iex.Connect: IEXException
+
+import Dates: Date
+import DataFrames: DataFrame, Not
+
+
+const valid_range_values = [
+    "max", "5y", "2y", "1y", "ytd", "6m", "3m", "1m", "1mm", "5d", "5dm",
+    "date", "dynamic"
+]
 
 
 """
-    chart(conn::Connection, symbol::String; daterange="1m")
+    history(conn::Connection, symbol::String; daterange="1m",
+        chartcloseonly::Bool = false,
+        columns = "date,open,high,low,close,volume,uOpen,uHigh,uLow,uClose,uVolume"
+    )
 
-Gets Returns adjusted and unadjusted historical data
+Gets adjusted and unadjusted historical data
 for up to 15 years. Useful for building charts.
 
 This endpoint is carried over from the IEX 1.0 API,
 and we know it has become difficult to use. We are
 working on new endpoints to simplify time series queries.
 """
-function history(conn::Connection, symbol::String; daterange = "1m")
+function history(
+    conn::Connection,
+    symbol::String;
+    daterange = "1m",
+    chartcloseonly::Bool = false,
+    columns = "date,open,high,low,close,volume,uOpen,uHigh,uLow,uClose,uVolume"
+)::DataFrame
+    if !(daterange in valid_range_values)
+        throw(ArgumentError("invalid range value"))
+    end
     path_params = Dict("symbol" => symbol, "range" => daterange)
-    d = get_stocks(conn, "chart", path_params)
-    df = vcat(map(x -> DataFrame(x), d)...)
-    df[!, :date] = Date.(df[!, :date])
-    cols = [
-        :date,
-        :open,
-        :high,
-        :low,
-        :close,
-        :volume,
-        :uOpen,
-        :uHigh,
-        :uLow,
-        :uClose,
-        :uVolume,
-    ]
-    return df[!, cols]
+    query_params = Dict("chartCloseOnly" => chartcloseonly,
+        "filter" => columns
+    )
+    d = get_stocks(conn, "chart", path_params; query_params = query_params)
+    colnames = split("symbol," * columns, ",")
+    if length(d) > 0
+        df = vcat(map(x -> DataFrame(x), d)...)
+        df[!, :date] = Date.(df[!, :date])
+        df[!, :symbol] .= symbol
+        return df[!, colnames]
+    else
+        coltypes = [
+            String,
+            Date,
+            Float64,
+            Float64,
+            Float64,
+            Float64,
+            Int64,
+            Float64,
+            Float64,
+            Float64,
+            Float64,
+            Int64,
+        ]
+        return DataFrame(coltypes, colnames, 0)
+    end
 end
 
 
 """
     batch(conn::Connection, symbol::String, types::String; symbols="",
-        daterange="1m")
+        daterange="1m",
+        columns = "date,open,high,low,close,volume,uOpen,uHigh,uLow,uClose,uVolume"
+)
 
 Batch various API endpoints for a symbol or list of symbols
 
@@ -69,8 +100,8 @@ Query String Parameters
 |*       | Optional
 |        | Parameters that are sent to individual endpoints can be specified
 |        | in batch calls and will be applied to each supporting endpoint.
-|        | For example, last can be used for the news endpoint to specify the number
-|        | of articles
+|        | For example, last can be used for the news endpoint to specify the
+|        | number of articles
 """
 function batch(
     conn::Connection,
@@ -78,13 +109,21 @@ function batch(
     types::String;
     symbols::String = "",
     daterange = "1m",
+    columns = "date,open,high,low,close,volume,uOpen,uHigh,uLow,uClose,uVolume"
 )
 
     path_params = Dict("symbol" => symbol)
-    query_params = Dict("range" => daterange, "types" => types)
+
+    if !(daterange in valid_range_values)
+        throw(ArgumentError("invalid range value"))
+    end
+    query_params = Dict("range" => daterange, "types" => types,
+        "filter" => columns
+    )
+
     if symbol == "market"
         if isempty(symbols)
-            throw(UndefVarError("batch requires at least 1 symbol"))
+            throw(ArgumentError("batch requires at least 1 symbol"))
         else
             query_params = merge(Dict("symbols" => symbols), query_params)
         end
@@ -102,4 +141,35 @@ Response includes data from deep and quote. Refer to each endpoint for details.
 function book(conn::Connection, symbol::String)
     path_params = Dict("symbol" => symbol)
     return get_stocks(conn, "book", path_params)
+end
+
+
+"""
+    symbol_quote(conn::Connection, symbol::String; field::String="latestPrice")
+"""
+function symbol_quote(conn::Connection, symbol::String; field::String="latestPrice")
+    path_params = Dict("symbol" => symbol, "field" => field)
+    return get_stocks(conn, "quote", path_params)
+end
+
+
+"""
+    price(conn::Connection, symbol::String)
+
+Response includes data from deep and quote. Refer to each endpoint for details.
+"""
+function price(conn::Connection, symbol::String)
+    path_params = Dict("symbol" => symbol)
+    return get_stocks(conn, "price", path_params)
+end
+
+
+"""
+    ohlc(conn::Connection, symbol::String)
+
+Response includes data from deep and quote. Refer to each endpoint for details.
+"""
+function ohlc(conn::Connection, symbol::String)
+    path_params = Dict("symbol" => symbol)
+    return get_stocks(conn, "ohlc", path_params)
 end
