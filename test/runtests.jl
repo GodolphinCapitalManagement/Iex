6c1b4095e5756b62820188767275ff2f19cf139a
Gyan Sinha <gsinha@godolphincapital.com>
2020-07-27 13:42:51 -0400

    added sandbox token for tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,7 +1,9 @@
 using Iex
 using Test
 
+
 @testset "Iex.jl" begin
+    ENV["IEX_SANDBOX_KEY"] = "Tpk_6bb49ecaaecf4cbc97c5e83e8771c353"
     include("Profiles_test.jl")
     include("Prices_test.jl")
 end
