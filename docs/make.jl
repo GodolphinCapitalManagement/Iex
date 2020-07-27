edf1a15556bd7312ad6df100fb2972f5d0f40ec8
Gyan Sinha <gsinha@godolphincapital.com>
2020-07-26 20:49:51 -0400

    added deploydocs

--- docs/make.jl
+++ docs/make.jl
@@ -1,4 +1,16 @@
 using Documenter, Iex
 
-makedocs(sitename="Julia Inteface for IEX Cloud API")
+CI_FLG = get(ENV, "CI", nothing) == "true"
 
+makedocs(
+    modules = [Iex],
+    format = Documenter.HTML(
+        prettyurls = CI_FLG
+    ),
+    sitename="IexJulia")
+
+
+deploydocs(
+    repo = "github.com/GodolphinCapitalManagement/Iex.git",
+    push_preview = true,
+)
