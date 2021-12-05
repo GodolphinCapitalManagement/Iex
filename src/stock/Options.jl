7e7fbec4342333820d9e88c1641d8487f5d34466
Gyan Sinha <gyanorama@gmail.com>
2021-12-05 11:40:50 -0500

    added new functions for MV delta calculations

--- src/stock/Options.jl
+++ src/stock/Options.jl
@@ -1,8 +1,13 @@
 # Option Pricing Formulas
+using DataFrames
 using Distributions
 using Roots
 using Dates
+using RobustModels
+using GLM
+using ShiftedArrays
 
+using BLPData
 
 """
     bsopm_call(S, K, r, σ, t)
@@ -95,3 +100,75 @@ function calc_hr(S, K, r, σ, t, a, b, c)
 
     return (S=S, value=value, δ=δ, vega=vega, δₘᵥ=δₘᵥ)
 end  # function calc_hr
+
+
+"""
+    fortify_mv_df(dyn_df::DataFrame; K::Float64=11.5, r::Float64=0.0008)
+
+generates price data and computes min-variance deltas for a stock/warrant pair.
+"""
+function fortify_mv_df!(dyn_df::DataFrame; K::Float64=11.5, r::Float64=0.0008)
+
+    # create phase variables
+    transform!(dyn_df, [:date, :dadate, :closingdate] => ByRow((x,y,z) -> ismissing(y) & ismissing(z) || (x < y) ? 1 :
+        ifelse(x >= y && (x < z), 2, 3)) => :phase)
+
+    dyn_df[!, :ls] = lag(dyn_df.s)
+    dyn_df[!, :ds] = dyn_df.s .- dyn_df.ls 
+        
+    dyn_df[!, :lw] = lag(dyn_df.w)
+    dyn_df[!, :dw] = dyn_df.w .- dyn_df.lw
+
+    z = DataFrame(map(row -> calc_term(row.date, row.id, row.ipodate, enddate=row.enddate, 
+            dadate=row.dadate, mergerdate=row.mergerdate, closingdate=row.closingdate), 
+            eachrow(dyn_df)))
+
+    dyn_df = leftjoin!(dyn_df, z, on=[:id, :date])
+    select!(dyn_df, Not([:id, :ipodate, :dadate, :closingdate, :enddate, :mergerdate, :Nstep, :Estep]))
+
+    # fortify data with bsopm delta and vega
+    transform!(dyn_df, [:w, :s, :t] => ByRow( (x, y, z) -> bsopm_impvol(x, y, K, r, z)) => :σ)
+    transform!(dyn_df, [:s, :σ, :t] => ByRow( (a, b, c) -> !isnothing(b) ? 
+        bsopm_call(a, K, r, b, c) : (value=missing, δ=missing, vega=missing)) => [:value, :δ, :vega])
+
+    return dyn_df
+end  # function fortify_mv_df!
+
+
+"""
+    compute_mv_delta(dyn_df::DataFrame;
+        window::Union{Missing,Int64}=missing, robust::Bool=true)
+
+computes min-variance deltas for a stock/warrant pair.
+"""
+function compute_mv_delta(dyn_df::DataFrame;
+        window::Union{Missing,Int64}=missing, robust::Bool=true)
+
+    if ismissing(window)
+        window = nrow(dyn_df)
+    end
+
+    sub_df = last(dyn_df, window)
+    Y = sub_df.dw .- sub_df.δ .* sub_df.ds
+    μ = (sub_df.vega ./ sqrt.(sub_df.t)) .* (sub_df.ds./sub_df.s)
+    x₁ = μ
+    x₂ = μ .* sub_df.δ
+    x₃ = μ .* sub_df.δ.^2
+
+    if robust 
+        hw_reg = rlm(@formula(Y ~ -1 + x₁ + x₂ + x₃), 
+            DataFrame(Y=Y, x₁=x₁, x₂=x₂, x₃=x₃),
+            MEstimator{HuberLoss}(); method=:chol, 
+            initial_scale=15.0)
+    else
+        hw_reg = lm(@formula(Y ~ -1 + x₁ + x₂ + x₃), 
+            DataFrame(Y=Y, x₁=x₁, x₂=x₂, x₃=x₃))
+    end
+
+    a, b, c = coef(hw_reg)
+    sub_df[!, :δₘᵥ] = max.(0.0, sub_df.δ .+ (sub_df.vega ./ 
+        (sub_df.s .* sqrt.(sub_df.t))) .* (a .+ b .* sub_df.δ + c .* sub_df.δ.^2))
+
+    return (sub_df=sub_df, hw_reg=hw_reg, params=(a, b, c))
+
+end  # function compute_mv_delta
